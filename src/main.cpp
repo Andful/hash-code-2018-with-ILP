35e3052ba7ab38d04c871919a5aee1be0d6578a6
andful <buongiorno19972@gmail.com>
2019-03-29 20:50:14 +0100

    checkpoint

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,13 @@
 using Eigen::ArrayXXi;
 using Eigen::Dynamic;
 using Eigen::Array;
+
+char varname[256];
+
 int main1(int argn, char** argv)
 {
   std::ifstream in;
   in.open(argv[1]);
-
-  char varname[50];
 
   std::cout << "loading file:" << argv[1] << std::endl;
 
@@ -63,19 +64,17 @@ int main1(int argn, char** argv)
   ArrayXXi early_end_turn = early_start_turn + length_trip;
   ArrayXXi max_delay = late_start_turn - early_start_turn;
 
+  ArrayXXi start_distance = ArrayXXi::Zero(n_rides,2);
   ArrayXXi distance = ArrayXXi::Zero(n_rides,n_rides);
-
-  for (int i=0;i<n_rides;i++) {
-    for (int j=0;j<n_rides;j++) {
-      distance(i,j) = (end_pos.row(i) - start_pos.row(j)).abs().sum();
-    }
-  }
-
   Array<bool,Dynamic,Dynamic> valid_transition = Array<bool,Dynamic,Dynamic>::Zero(n_rides,n_rides);
 
   for (int i=0;i<n_rides;i++) {
+    start_distance(i) = start_pos(i,0) + start_pos(i,1);
+
     for (int j=0;j<n_rides;j++) {
-      if (early_end_turn(i) + distance(i,j) <= late_start_turn(j) and i != j) {
+      distance(i,j) = (end_pos.row(i) - start_pos.row(j)).abs().sum();
+
+      if (early_end_turn(i) + distance(i,j) <= late_start_turn(j) && i != j) {
           valid_transition(i,j) = true;
       }
     }
@@ -85,22 +84,26 @@ int main1(int argn, char** argv)
   GRBModel m = GRBModel(env);
 
   Array<GRBVar,Dynamic,Dynamic> start_transition(n_rides,1);
+  Array<GRBVar,Dynamic,Dynamic> end_transition(n_rides,1);
+  Array<GRBVar,Dynamic,Dynamic> transition(n_rides,n_rides);
+  Array<GRBVar,Dynamic,Dynamic> delay(n_rides,1);
+  Array<GRBVar,Dynamic,Dynamic> bonus(n_rides,1);
+
 
   for (int i=0;i<n_rides;i++) {
+
     sprintf(varname,"t_S_%d",i);
     start_transition(i) = m.addVar(0.0,1.0,0.0,GRB_BINARY,varname);
-  }
 
-  Array<GRBVar,Dynamic,Dynamic> end_transition(n_rides,1);
-
-  for (int i=0;i<n_rides;i++) {
     sprintf(varname,"t_%d_E",i);
     end_transition(i) = m.addVar(0.0,1.0,0.0,GRB_BINARY,varname);
-  }
 
-  Array<GRBVar,Dynamic,Dynamic> transition(n_rides,n_rides);
+    sprintf(varname,"d_%d",i);
+    delay(i) = m.addVar(0.0,max_delay(i),0.0,GRB_INTEGER,varname);
 
-  for (int i=0;i<n_rides;i++) {
+    sprintf(varname,"bonus_%d",i);
+    bonus(i) = m.addVar(0.0,1.0,0.0,GRB_BINARY,varname);
+
     for (int j=0;j<n_rides;j++) {
       if (valid_transition(i,j)) {
         sprintf(varname,"t_%d_%d",i,j);
@@ -109,70 +112,84 @@ int main1(int argn, char** argv)
     }
   }
 
-  Array<GRBVar,Dynamic,Dynamic> delay(n_rides,1);
-
-  for (int i=0;i<n_rides;i++) {
-    sprintf(varname,"d_%d",i);
-    delay(i) = m.addVar(0,max_delay(i),0.0,GRB_INTEGER,varname);
+  {
+    GRBLinExpr start = 0;
+    for (int i=0;i<n_rides;i++) {
+      start += start_transition(i);
+    }
+    m.addConstr(start <= n_vehic, "start_constrain");
   }
 
-  Array<GRBVar,Dynamic,Dynamic> bonus(n_rides,1);
-
   for (int i=0;i<n_rides;i++) {
-    sprintf(varname,"bonus_%d",i);
-    bonus(i) = m.addVar(0.0,1.0,0.0,GRB_BINARY,varname);
-  }
+    GRBLinExpr incoming_transitions = 0;
+    GRBLinExpr outgoing_transitions = 0;
 
-  GRBLinExpr ls;
-  GRBLinExpr rs;
-
-  for (int i=0;i<n_rides;i++) {
-    ls = 0;
-    rs = 0;
-    ls += start_transition(i);
+    incoming_transitions += start_transition(i);
     for (int j=0;j<n_rides;j++) {
       if (valid_transition(j,i)) {
-        ls += transition(j,i);
+        incoming_transitions += transition(j,i);
       }
     }
 
-    rs += end_transition(i);
+    outgoing_transitions += end_transition(i);
     for (int j=0;j<n_rides;j++) {
       if (valid_transition(i,j)) {
-        ls += transition(i,j);
+        outgoing_transitions += transition(i,j);
       }
     }
-    sprintf(varname,"io_euqal_%d",i);
-    m.addConstr(ls == rs, varname);
 
     sprintf(varname,"at_most_one_%d",i);
-    m.addConstr(ls == 1, varname);
-  }
+    m.addConstr(incoming_transitions <= 1, varname);
 
-  for (int i=0;i<n_rides;i++) {
+    sprintf(varname,"io_euqal_%d",i);
+    m.addConstr(incoming_transitions == outgoing_transitions, varname);
+
+    sprintf(varname,"bonus_if_ride_taken_%d",i);
+    m.addConstr(incoming_transitions >= bonus(i), varname);
+
+    sprintf(varname,"bonus_apply_%d",i);
+    m.addConstr((1 - bonus(i)) * max_delay(i) >= delay(i),varname);
+
+    sprintf(varname,"delay_S_%d",i);
+    m.addConstr(
+      early_start_turn(i) +
+      delay(i) +
+      (1-start_transition(i))*(start_distance(i) -
+      early_start_turn(i) -
+      max_delay(i))
+      >=
+      start_distance(i)
+      ,varname);
+
     for (int j=0;j<n_rides;j++) {
       if (valid_transition(i,j)) {
         sprintf(varname,"delay_%d_%d",i,j);
-        m.addConstr(early_end_turn(i)+delay(i)+distance(i,j) <= early_start_turn(j) + delay(j) + (1-transition(i,j))*(max_delay(i)+distance(i,j)), varname);
+        m.addConstr(
+          early_end_turn(i) +
+          delay(i) +
+          distance(i,j)
+          <=
+          early_start_turn(j) +
+          delay(j) +
+          (1-transition(i,j))*
+          (max_delay(i) + distance(i,j) + early_end_turn(i) - early_start_turn(j))
+          ,varname);
       }
     }
   }
 
-  for (int i=0;i<n_rides;i++) {
-    sprintf(varname,"bonus_apply_%d",i);
-    m.addConstr((1 - bonus(i))*max_delay(i) >= delay(i),varname);
-  }
 
   GRBLinExpr objective = 0;
 
   for(int i=0;i<n_rides;i++) {
     objective+= bonus_point*bonus(i);
+    objective += start_transition(i) * length_trip(i);
+
     for(int j=0;j<n_rides;j++) {
       if (valid_transition(i,j)) {
         objective+= length_trip(j)*transition(i,j);
       }
     }
-    objective += start_transition(i) * length_trip(i);
   }
 
   m.setObjective(objective,GRB_MAXIMIZE);
